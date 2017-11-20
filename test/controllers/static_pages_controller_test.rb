f9e72e4f7abeb7c9c7b084a83e4c45211e046bfb
tamaraz <tamara.zlender@gmail.com>
2017-11-20 13:21:52 -0500

    Refactor html code, Add .ruby-gemset and .ruby-version files.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,26 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  #Test gets a URL and verifies (via an assertion) that the result is a success.
+  def setup
+    @base_title = 'Ruby on Rails Tutorial Sample App'
+  end
+  # Test gets a URL and verifies (via an assertion) that the result is a success.
   # Here the use of get indicates that our tests expect the Home page
   # to be ordinary web page, accessed using a GET request.
   # The response :success is an abstract representation of the underlying
   # HTTP status code (in this case, 200 OK).
-  test "should get home" do
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', "Home | #{@base_title}"
   end
 
-  test "should get help" do
+  test 'should get help' do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
-  test "should get about" do
+  test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', "About | #{@base_title}"
   end
 end
